b878ddcf0c35758b62ee89a0e917d458d770b721
Averizer <emilianog628@gmail.com>
2022-07-28 00:48:13 -0500

    Update FS Dev

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 window.onresize = resize
 nombreJunto = document.getElementsByClassName("nombre-junto")
 nombreSeparado = document.getElementsByClassName("nombre-separado")
+developer = document.getElementsByClassName("developer")
+
 function resize() {
     if (window.innerWidth < 1380) {
         nombreJunto[0].style.display = "block";
         nombreJunto[1].style.display = "block";
+        developer[0].style.display = "block";
 
         for (i = 0; i < 16; i++) nombreSeparado[i].style.display = "none";
     } else {
         nombreJunto[0].style.display = "none";
         nombreJunto[1].style.display = "none";
+        developer[0].style.display = "inline";
 
         for (i = 0; i < 16; i++) nombreSeparado[i].style.display = "inline";
     }
